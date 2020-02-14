7fc79720fee52a0730b42da04847a43477dc046c
Alexandre Fourcat <afourcat@gmail.com>
2020-02-14 11:15:19 +0100

    :zap: Scaffold of the program.

--- src/dot_vox.rs
+++ src/dot_vox.rs
@@ -4,7 +4,14 @@
 // Description:
 // Create a Generic Octree from a dot_vox file.
 
-use dot_vox;
+use dot_vox::DotVoxData;
+use crate::Octree;
+
+impl<T> From<DotVoxData> for Octree<T, (u8, u8, u8)> {
+    fn from(data: DotVoxData) -> Octree<T, (u8, u8, u8)> {
+        Octree::new()
+    }
+}
 
 #[cfg(test)]
 mod test {
