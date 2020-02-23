841c38be23f57575a3b009dc61780d2f7d63b895
Alexandre Fourcat <afourcat@gmail.com>
2020-02-23 17:00:29 +0100

    :sparkles: Adding basic import.

--- src/dot_vox.rs
+++ src/dot_vox.rs
@@ -5,6 +5,7 @@
 // Create a Generic Octree from a dot_vox file.
 //
 
+use crate::aabb::AABB;
 use crate::octree::LocCode;
 use crate::Octree;
 use dot_vox::DotVoxData;
@@ -16,7 +17,7 @@ pub(crate) fn vox_to_octrees<L: LocCode>(data: DotVoxData) -> Vec<Octree<L, u32>
     let mut octrees = Vec::with_capacity(1);
 
     for (i, model) in data.models.iter().enumerate() {
-        let octree = Octree::new();
+        let mut octree = Octree::new();
         println!(
             "Model {} of size: x: {} y: {} z: {}.",
             i, model.size.x, model.size.y, model.size.z
@@ -28,6 +29,21 @@ pub(crate) fn vox_to_octrees<L: LocCode>(data: DotVoxData) -> Vec<Octree<L, u32>
                 voxel.y,
                 voxel.z,
                 data.palette[voxel.i as usize] // TODO: maybe do a safe version.
+            );
+            let (x, y, z) = (
+                voxel.x as f64 / model.size.x as f64,
+                voxel.y as f64 / model.size.y as f64,
+                voxel.z as f64 / model.size.z as f64,
+            );
+            let (x1, y1, z1) = (
+                x + 1.0 / model.size.x as f64,
+                y + 1.0 / model.size.y as f64,
+                z + 1.0 / model.size.z as f64,
+            );
+            println!("Data: {:#?}", AABB::<L>::new(x, y, z, x1, y1, z1));
+            octree.merge(
+                AABB::<L>::new(x, y, z, x1, y1, z1),
+                data.palette[voxel.i as usize],
             );
         }
         octrees.push(octree);
