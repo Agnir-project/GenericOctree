fef2293dfa72c9ec6048f49e1afbe3973f1f51ff
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-03-02 14:05:26 +0100

    :bug: Remove all features from main example

--- examples/main.rs
+++ examples/main.rs
@@ -2,16 +2,6 @@ use color::Rgb;
 use generic_octree::{Octree, AABB};
 
 fn main() {
-    let octree = Octree::<u64, u64>::from_dotvox("./examples/monu10.vox", 21);
-
-    println!(
-        "{:?}",
-        octree
-            .unwrap()
-            .into_iter()
-            .map(|tree| tree.depth())
-            .collect::<Vec<u32>>()
-    );
     let mut tree: Octree<u64, Rgb<u8>> = Octree::new(21);
     let data: Vec<AABB> = AABB::new(0.0, 0.0, 0.0, 1.0, 0.5, 0.75).explode((0.5, 0.5, 0.5));
     println!(
